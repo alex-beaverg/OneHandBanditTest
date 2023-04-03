ca319b611b92931149111e7369735ecc9ee193b1
Alexey Bobrikov <a.beaverg@gmail.com>
2023-04-03 12:26:33 +0300

    add unittests

--- features/steps/one_hand_bandit_test_behave.py
+++ features/steps/one_hand_bandit_test_behave.py
@@ -7,123 +7,93 @@ import waiters
 
 @given('website "{url}" with "{brows}" browser')
 def step(context: webdriver, url: str, brows: str) -> None:
-    # Run webdriver
     if brows == 'edge':
         context.browser = webdriver.Edge()
     elif brows == 'chrome':
         context.browser = webdriver.Chrome()
     elif brows == 'firefox':
         context.browser = webdriver.Firefox()
-    # Maximize window
     context.browser.maximize_window()
-    # Open page with getting URL
     context.browser.get(url)
 
 
 @then('index page include text "{text}"')
 def step(context: webdriver, text: str) -> None:
-    # Synchronisation
     waiters.wait_until_page_loaded(context, '//*[contains(text(), "%s")]', text)
-    # Check if the page was opened
     assert context.browser.find_element(By.XPATH, '//*[contains(text(), "%s")]' % text)
 
 
 @then('the game can be played')
 def step(context: webdriver) -> None:
-    # Click the button to play the game
     context.browser.find_element(By.ID, 'btn').click()
-    # Check if the button was clicked
     assert context.browser.find_element(By.XPATH, '//*[contains(text(), "Moves:")]')
 
 
 @then('the game can be won')
 def step(context: webdriver) -> None:
-    # Play the game to win
     while True:
         try:
-            # Check if the game was won
             context.browser.find_element(By.XPATH, '//*[contains(text(), "You WON")]')
         except NoSuchElementException:
-            # Do next move if the game wasn't won
             context.browser.find_element(By.ID, 'btn').click()
         else:
-            # Exit when the game was won
             break
 
 
 @then('you can see your results')
 def step(context: webdriver) -> None:
-    # Check if result string exists
     assert context.browser.find_element(By.XPATH, '//*[contains(text(), "1st game")]')
 
 
 @then('you can play again')
 def step(context: webdriver) -> None:
-    # Play the game to win again
     play = context.browser.find_element(By.ID, 'btn')
     play.click()
     while True:
         try:
-            # Check if the game was won
             context.browser.find_element(By.XPATH, '//*[contains(text(), "You WON")]')
         except NoSuchElementException:
-            # Do next move if the game wasn't won
             play.click()
         else:
-            # Exit when the game was won
             break
 
 
 @then('you can see your next results')
 def step(context: webdriver) -> None:
-    # Check if result string exists
     assert context.browser.find_element(By.XPATH, '//*[contains(text(), "2nd game")]')
 
 
 @then('you can delete your results')
 def step(context: webdriver) -> None:
-    # Delete result
     context.browser.find_element(By.ID, 'reload').click()
     try:
-        # Check if the button "DELETE" exists
         context.browser.find_element(By.ID, 'reload')
     except NoSuchElementException:
-        # Pass if the button "DELETE" wasn't found
         pass
     else:
-        # Fail if the button "DELETE" was found
         raise AssertionError
 
 
 @then('you can read the rules of the game')
 def step(context: webdriver) -> None:
-    # Click the button "RULES"
     context.browser.find_element(By.XPATH, '//*[contains(text(), "RULES")]').click()
-    # Check if the button was clicked
     assert context.browser.find_element(By.XPATH, '//*[contains(text(), "That\'s all. Good Luck!")]')
 
 
 @then('you can click to "START GAME" button')
 def step(context: webdriver) -> None:
-    # Click the button "START GAME"
     context.browser.find_element(By.XPATH, '//*[contains(@value, "START")]').click()
-    # Check if the button was clicked
     assert context.browser.find_element(By.XPATH, '//*[contains(text(), "Good Luck, My Friend!")]')
 
 
 @then('you can read "ABOUT" page')
 def step(context: webdriver) -> None:
-    # Click the button "ABOUT"
     context.browser.find_element(By.XPATH, '//*[contains(text(), "ABOUT")]').click()
-    # Check if the button was clicked
     assert context.browser.find_element(By.XPATH, '//*[contains(text(), "Alexey Bobrikov")]')
 
 
 @then('you can return to the home page')
 def step(context: webdriver) -> None:
-    # Click the button "GAME"
     context.browser.find_element(By.XPATH, '//*[contains(text(), "GAME")]').click()
-    # Check if the button was clicked
     assert context.browser.find_element(By.XPATH, '//*[contains(text(), "Good Luck, My Friend!")]')
-    # Close webdriver
     context.browser.quit()
--- test_one_hand_bandit_unittest.py
+++ test_one_hand_bandit_unittest.py
@@ -10,12 +10,12 @@ class TestOneHandBandit(unittest.TestCase):
     driver.get('https://one-hand-bandit.vercel.app/')
     play = driver.find_element(By.ID, 'btn')
 
-    def test_1_open_home_page(self) -> None:
+    def test_01_open_home_page(self) -> None:
         """Testing of opening home page"""
         result = self.driver.find_element(By.ID, 'wish').text
         self.assertEqual('Good Luck, My Friend!', result)
 
-    def test_2_play_1_game(self) -> None:
+    def test_02_play_1_game(self) -> None:
         """Testing of playing game to first win"""
         self.play.click()
         while True:
@@ -28,12 +28,12 @@ class TestOneHandBandit(unittest.TestCase):
         result = self.driver.find_element(By.XPATH, '//*[contains(text(), "You WON")]').text[:8]
         self.assertEqual('You WON!', result)
 
-    def test_3_results_1_game(self) -> None:
+    def test_03_results_1_game(self) -> None:
         """Testing results of first game"""
         result = self.driver.find_element(By.XPATH, '//*[contains(text(), "1st game")]').text[:8]
         self.assertEqual('1st game', result)
 
-    def test_4_play_2_game(self) -> None:
+    def test_04_play_2_game(self) -> None:
         """Testing of playing game to second win"""
         self.play.click()
         while True:
@@ -46,12 +46,12 @@ class TestOneHandBandit(unittest.TestCase):
         result = self.driver.find_element(By.XPATH, '//*[contains(text(), "You WON")]').text[:8]
         self.assertEqual('You WON!', result)
 
-    def test_5_results_2_game(self) -> None:
+    def test_05_results_2_game(self) -> None:
         """Testing results of second game"""
         result = self.driver.find_element(By.XPATH, '//*[contains(text(), "2nd game")]').text[:8]
         self.assertEqual('2nd game', result)
 
-    def test_6_play_3_game(self) -> None:
+    def test_06_play_3_game(self) -> None:
         """Testing of playing game to third win"""
         self.play.click()
         while True:
@@ -64,9 +64,43 @@ class TestOneHandBandit(unittest.TestCase):
         result = self.driver.find_element(By.XPATH, '//*[contains(text(), "You WON")]').text[:8]
         self.assertEqual('You WON!', result)
 
-    def test_7_results_3_game(self) -> None:
+    def test_07_results_3_game(self) -> None:
         """Testing results of third game"""
         result = self.driver.find_element(By.XPATH, '//*[contains(text(), "3rd game")]').text[:8]
         self.assertEqual('3rd game', result)
 
+    def test_08_delete_results(self) -> None:
+        """Testing deleting results"""
+        self.driver.find_element(By.ID, 'reload').click()
+        try:
+            self.driver.find_element(By.ID, 'reload')
+        except NoSuchElementException:
+            pass
+        else:
+            raise AssertionError
+        self.assertEqual(self.driver.find_element(By.ID, 'btn'),
+                         self.driver.find_element(By.XPATH, '//*[@value="PLAY"]'))
+
+    def test_09_open_rules_page(self) -> None:
+        """Testing opening RULES page"""
+        self.driver.find_element(By.XPATH, '//*[contains(text(), "RULES")]').click()
+        self.assertEqual(self.driver.find_element(By.TAG_NAME, 'h1').text[:9], 'The rules')
+
+    def test_10_check_btn_start_game(self) -> None:
+        """Testing button 'START GAME'"""
+        self.driver.find_element(By.XPATH, '//*[contains(@value, "START GAME")]').click()
+        result = self.driver.find_element(By.ID, 'wish').text
+        self.assertEqual('Good Luck, My Friend!', result)
+
+    def test_11_open_page_about(self) -> None:
+        """Testing opening ABOUT page"""
+        self.driver.find_element(By.XPATH, '//*[contains(text(), "ABOUT")]').click()
+        self.assertTrue(
+            self.driver.find_element(By.XPATH, '//*[contains(text(), "Alexey Bobrikov")]').text.startswith('1. Design'))
+
+    def test_12_return_home_page(self) -> None:
+        """Testing returning to the home page"""
+        self.driver.find_element(By.XPATH, '//*[contains(text(), "GAME")]').click()
+        result = self.driver.find_element(By.ID, 'wish').text
+        self.assertEqual('Good Luck, My Friend!', result)
         self.driver.close()
