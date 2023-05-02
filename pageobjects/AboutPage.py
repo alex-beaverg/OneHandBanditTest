ea4e45cd55d3c08b69f4aa33fedaa259ab860c3c
Alexey Bobrikov <a.beaverg@gmail.com>
2023-05-02 18:49:03 +0300

    Remove exceptions. Add access modifiers

--- pageobjects/AboutPage.py
+++ pageobjects/AboutPage.py
@@ -1,21 +1,32 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class AboutPage:
     """Docstring: Class pageobjects.AboutPage"""
     def __init__(self, driver) -> None:
         """Docstring: Constructor for class pageobjects.AboutPage"""
-        self.driver = driver
+        self.__driver = driver
         # Webelements and variables:
-        self.ap_game_menu_item = self.driver.find_element(By.XPATH, '//*[contains(text(), "GAME")]')
-        self.ap_rules_menu_item = self.driver.find_element(By.XPATH, '//*[contains(text(), "RULES")]')
-        self.ap_real_text = 'Information'
-        self.ap_find_text = self.driver.find_element(By.TAG_NAME, 'h1').text[:11]
+        self.__ap_game_menu_item = self.__driver.find_element(By.XPATH, '//*[contains(text(), "GAME")]')
+        self.__ap_rules_menu_item = self.__driver.find_element(By.XPATH, '//*[contains(text(), "RULES")]')
+        self.__ap_real_text = 'Information'
+        self.__ap_find_text = self.__driver.find_element(By.TAG_NAME, 'h1').text[:11]
 
+    # Getters:
+    def get_ap_real_text(self) -> str:
+        """Docstring: Getter for ap_real_text variable"""
+        return self.__ap_real_text
+
+    def get_ap_find_text(self) -> WebElement:
+        """Docstring: Getter for ap_find_text webElement"""
+        return self.__ap_find_text
+
+    # Methods:
     def ap_click_game_menu_item(self) -> None:
         """Docstring: Method to click GAME menu item on the ABOUT page"""
-        return self.ap_game_menu_item.click()
+        return self.__ap_game_menu_item.click()
 
     def ap_click_rules_menu_item(self) -> None:
         """Docstring: Method to click RULES menu item on the ABOUT page"""
-        return self.ap_rules_menu_item.click()
+        return self.__ap_rules_menu_item.click()
